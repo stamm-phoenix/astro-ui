cd51ff912191f2e84d18b1cbeb1cf4565297580b
hugo-berendi <git@hugo-berendi.de>
2025-10-24 10:55:37 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -5,41 +5,43 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          nodejs_22
-          nodePackages.pnpm
-        ];
-      };
-
-      packages.${system}.dpsg-phoenix-ui = pkgs.stdenv.mkDerivation {
-        pname = "dpsg-phoenix-ui";
-        version = "0.0.1"; # This should ideally be read from package.json
-
-        src = self;
-
-        buildInputs = with pkgs; [
-          nodejs_22
-          nodePackages.pnpm
-        ];
-
-        installPhase = ''
-          pnpm install --frozen-lockfile
-          pnpm run build:lib
-          mkdir -p $out
-          cp -r dist $out/
-          cp -r package.json $out/
-          cp -r README.md $out/
-          cp -r LICENSE $out/
-        '';
-      };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
     };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        nodejs_22
+        nodePackages.pnpm
+      ];
+    };
+
+    packages.${system}.astro-ui = pkgs.stdenv.mkDerivation {
+      pname = "dpsg-phoenix-ui";
+      version = "0.0.1"; # This should ideally be read from package.json
+
+      src = self;
+
+      buildInputs = with pkgs; [
+        nodejs_22
+        nodePackages.pnpm
+      ];
+
+      installPhase = ''
+        pnpm install --frozen-lockfile
+        pnpm run build:lib
+        mkdir -p $out
+        cp -r dist $out/
+        cp -r package.json $out/
+        cp -r README.md $out/
+        cp -r LICENSE $out/
+      '';
+    };
+  };
 }
+
