18392c424b6025e7376dd2caa5401d04a4eeaae7
hugo-berendi <git@hugo-berendi.de>
2025-10-24 08:54:08 +0200

    feat: Add Nix flake for Astro development with Node.js and pnpm

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Astro development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs_22
+          nodePackages.pnpm
+        ];
+      };
+    };
+}
