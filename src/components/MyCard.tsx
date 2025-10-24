eaf00685e6e38e12196e8c31cc6dc7c3048ff38a
hugo-berendi <git@hugo-berendi.de>
2025-10-24 09:12:58 +0200

    feat: Add example card to index page

---
+++ src/components/MyCard.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@/components/ui/card';
+
+interface MyCardProps {
+  title: string;
+  description: string;
+  content?: React.ReactNode;
+  footer?: React.ReactNode;
+}
+
+const MyCard: React.FC<MyCardProps> = ({ title, description, content, footer }) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      {content && <CardContent>{content}</CardContent>}
+      {footer && <CardFooter>{footer}</CardFooter>}
+    </Card>
+  );
+};
+
+export default MyCard;
